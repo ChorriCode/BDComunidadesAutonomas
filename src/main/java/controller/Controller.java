06e93edec42e926c8040f92ef67cdd2f2f4ee82f
JavierHS <JavierHS@DESKTOP-RQ3V18B>
2018-04-19 22:29:07 +0100

    funciona con metodo consultaPadronCAProv pero falta reparar el meetodo consultaPadronCAprov

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -19,6 +19,44 @@ public class Controller {
 		
 	}
 	
+	
+	public void consultaPadronCAProv(Model myModel, View myView, String sql) {
+		myModel.connectToBD();
+		sql = " \r\n" + 
+				"select CA as Comunidad, provincia, sum(padron) as padron from padron pa inner join provincias p1, comunidadesautonomas c1, municipios m1 where pa.CodMunicipio \r\n" + 
+				"= m1.CodMunicipio and m1.CodProvincia = p1.CodProvincia and p1.CodCA = c1.CodCA group by p1.CodProvincia order by c1.CA, p1.Provincia";
+		ResultSet rs = myModel.readOnBD(myModel.getDbName(), sql);
+		String anteriorCA = "0" ;
+		int subTotal = 0;
+		int total = 0;
+		try {
+			while(rs.next()) {
+				
+				if (!(anteriorCA.equals(rs.getString(1))) ) {				
+					if (subTotal != 0) {
+						System.out.println("SubTotal: " + subTotal + " de " + anteriorCA);
+					}
+					
+					System.out.println("\nCOMUNIDAD AUTONOMA: " + rs.getString(1));
+					anteriorCA = rs.getString(1);
+					total += subTotal;
+					subTotal = 0;
+				}
+					
+					subTotal += rs.getInt(3);
+					
+					System.out.println(rs.getString(2) + " - " + rs.getString(3));
+					
+			}
+			
+			System.out.println("España: " + (total + subTotal));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+	}
+	
 	public void solictarConsulta(Model myModel, View myView, String sql) {
 		
 		myModel.connectToBD();
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,11 +14,13 @@ public class Main {
 									"paro",
 									"com.mysql.jdbc.Driver", 
 									"root", 
-									"");
+									"Gratis007");
 		View myView = new View();
 		Controller myController = new Controller(myModel, myView);
 		String sql = "SELECT provincias.CodCA, comunidadesautonomas.CA, municipios.codProvincia, provincia, sum(padron) from municipios LEFT JOIN padron on municipios.CodMunicipio = padron.CodMunicipio LEFT JOIN provincias on municipios.CodProvincia = provincias.CodProvincia LEFT JOIN comunidadesautonomas on provincias.CodCA = comunidadesautonomas.CodCA GROUP BY CodProvincia, provincia, CA order by CodCA, provincia;";
-		myController.solictarConsulta(myModel, myView, sql);
+		
+		myController.consultaPadronCAProv(myModel, myView, sql);
+		//myController.solictarConsulta(myModel, myView, sql);
 		
 	}
 
